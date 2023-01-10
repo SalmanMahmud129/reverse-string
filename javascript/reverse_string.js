aa289ad07f5d3c3e7d240c043b683b98fda7f8f0
Salman Mahmud <salman_mahmud@hotmail.com>
2023-01-10 13:19:17 -0500

    commit tree 5da9df6e927b1a1018e2374c872057e709083109

--- javascript/reverse_string.js
+++ javascript/reverse_string.js
@@ -1,6 +1,24 @@
 function reverseString(str) {
   // type your code here
+  let reversedStr = []
+  // console.log("characters in reverse:")
+
+  // console.log('str.length-1', str.length-1)
+
+  for (let i = str.length - 1; i >= 0; i--){
+    console.log(i, str[i])
+    reversedStr.push(str[i])
+  }
+  console.log('reversedStr:', reversedStr.join(''))
+
+  return reversedStr.join('')
 }
+
+reverseString("hello")
+reverseString("your mom")
+reverseString("Testing")
+reverseString("I did it")
+
 
 if (require.main === module) {
   // add your own tests in here
@@ -17,3 +35,7 @@ module.exports = reverseString;
 
 // Please add your pseudocode to this file
 // And a written explanation of your solution
+// Create an empty array that will be filled in with the characters in reverse order
+//      iterate through the given string starting from the end of it
+//          push each character to the empty array
+// return the result
